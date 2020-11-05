86a8cb585496fe4625e2cb43e649b0c9b8fc1b32
EhrenW1 <ehren.wargo@patelhs.org>
2020-11-05 20:24:37 +0000

    I added an Array

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args)
+   {
+  String[ ] carBrands = {"Toyota", "Honda", "Ford", "Chevy", "Mazda", "Kia", "Jeep", "Saturn", "Subaru"};
+   
+   for(int i = 0; i < 7; i++){
+   System.out.println("The car brand is " + carBrands[i]);
+   }
+     
+  carBrands[6]= "Dodge";
+
+  for(int i = 0; i < 7; i++){ 
+    System.out.println( "The update is " + carBrands[i]);
+  }
   }
 }
+
+     
